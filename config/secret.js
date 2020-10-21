14d71f4452d20abfac55dfd667720ed9cd101c55
cholatrek <71100294+cholatrek@users.noreply.github.com>
2020-10-21 19:43:20 +0100

    Update secret.js

--- config/secret.js
+++ config/secret.js
@@ -1,5 +1,5 @@
 module.exports = {
     database : 'mongodb+srv://root:Kollybright150@cluster0-k6y69.mongodb.net/Greenlab-Microfactory?retryWrites=true&w=majority',
-    port: 3000
+    port: 9000
     }
     
